55ebbdc469d594d2baeb85d8046ad32685656e18
Nathan Reale <nathan.reale@gmail.com>
2015-04-04 17:01:57 -0700

    Clean up osx config

--- home/.zsh/osx.zsh
+++ home/.zsh/osx.zsh
@@ -1,15 +1,7 @@
 alias ls="ls -G"
 
 # Add directories to PATH
-# Add npm to the directory path
-# Add Google App Engine
-export PATH="$PATH:/usr/games:/Users/nathan/bin:/usr/local/share/npm/bin:/Users/nathan/bin/go_appengine"
-
-
-# Load RVM
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+export PATH="$PATH:/Users/nathan/bin"
 
 # Include the z script for intelligent directory jumping
 . $HOME/bin/z.sh
-
-export PATH=$HOME/.rvm/bin:$PATH # Add RVM to PATH for scripting
